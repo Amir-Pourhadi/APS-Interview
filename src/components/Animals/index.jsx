f595db86d4197d043737338cd8825a9b1ac7eede
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-13 03:24:45 +0430

    animals component updated

--- src/components/Animals/index.jsx
+++ src/components/Animals/index.jsx
@@ -1,14 +1,14 @@
 import PrimaryBtn from "../assets/PrimaryBtn";
 
-const Animals = () => {
+const Animals = ({ userName }) => {
   return (
     <div>
-      <p>Enter name of 3 animals separated by comma</p>
-      <form>
-        <label htmlFor="animal-input">My Animals: </label>
-        <input id="animal-input" type="text" />
-        <PrimaryBtn link="sort-list" />
-      </form>
+      <p>
+        <span>Well Done {userName} !</span> Enter name of 3 animals separated by comma
+      </p>
+      <label htmlFor="animal-input">My Animals: </label>
+      <input id="animal-input" type="text" />
+      <PrimaryBtn link="sort-list" />
     </div>
   );
 };
