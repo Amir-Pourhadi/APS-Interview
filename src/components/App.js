3c2bab24ce37f435398b606597ea60679c3e1c5c
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-12 16:27:22 +0430

    home component rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Switch } from "react-router";
 import { GlobalStyle } from "./GlobalStyle";
+import Home from "./Home";
 import SortList from "./SortList";
 
 const App = () => (
-  <>
+  <div className="container-fluid">
     <GlobalStyle />
     <Switch>
+      <Route exact path="/" component={Home} />
       <Route path="/sort-list" component={SortList} />
     </Switch>
-  </>
+  </div>
 );
 
 export default App;
