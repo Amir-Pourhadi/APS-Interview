b40916398060dbf30e9a5f30e14f4172fba1bfcd
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-13 03:44:11 +0430

    create object of animals

--- src/components/Animals/index.jsx
+++ src/components/Animals/index.jsx
@@ -1,13 +1,25 @@
+import { useRef } from "react";
 import PrimaryBtn from "../assets/PrimaryBtn";
 
-const Animals = ({ userName }) => {
+const Animals = ({ user: { name, animals }, updateUser }) => {
+  const inputEl = useRef("");
+
+  const handleUpdateUser = (e) => {
+    e.preventDefault();
+    const animals = inputEl.current.value.split(",").map((animal) => animal.trim());
+    updateUser({ animals });
+  };
+
   return (
     <div>
       <p>
-        <span>Well Done {userName} !</span> Enter name of 3 animals separated by comma
+        <span>Well Done {name} !</span> Enter name of 3 animals separated by comma
       </p>
-      <label htmlFor="animal-input">My Animals: </label>
-      <input id="animal-input" type="text" />
+      <form onSubmit={handleUpdateUser}>
+        <label htmlFor="animal-input">My Animals: </label>
+        <input id="animal-input" type="text" ref={inputEl} />
+        <input type="submit" className="btn btn-success" />
+      </form>
       <PrimaryBtn link="sort-list" />
     </div>
   );
