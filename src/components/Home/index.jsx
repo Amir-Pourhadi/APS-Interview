061f5423b1ba57c8eea1b5255b94154bdd6123da
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-13 19:21:05 +0430

    component updated

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,10 +2,12 @@ import PrimaryBtn from "../assets/PrimaryBtn";
 import { Main } from "./view";
 
 const Home = () => (
-  <Main>
-    <h1>Welcome to APSignlas English Interview</h1>
-    <p>You are going to answer some easy but nice questions 😁</p>
-    <p>Take a deep breath and press Continue...</p>
+  <Main className="text-center">
+    <h1 className="py-5">Welcome to APSignlas English Interview</h1>
+    <p className="py-5 fs-3">
+      You are going to answer some easy but nice questions. <br />
+      Take a deep breath and press Continue to start...
+    </p>
     <PrimaryBtn link="/name" />
   </Main>
 );
