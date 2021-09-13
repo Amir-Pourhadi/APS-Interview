bc04e3cb8fd3cb1304384cd43fa8954b4c776713
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-13 05:31:47 +0430

    Header created

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+import { HeaderTag } from "./view";
+
+const Header = () => <HeaderTag className="d-flex align-items-center justify-content-around">
+  Learning Coding Free is not a Dream anymore 😍
+</HeaderTag>;
+
+export default Header;
---
+++ src/components/Header/view.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const HeaderTag = styled.header({
+  height: 75,
+  boxShadow: "0 5px 10px -2px grey",
+});
