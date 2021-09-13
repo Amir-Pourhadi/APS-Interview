3c9c6729dd4791c03a98fab2a213085213070419
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-13 19:07:09 +0430

    Header updated

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,19 @@
-import { HeaderTag } from "./view";
+import { FaInstagram } from "react-icons/fa";
+import logo from "./logo.png";
+import { HeaderTag, Logo } from "./view";
 
-const Header = () => <HeaderTag className="d-flex align-items-center justify-content-around">
-  Learning Coding Free is not a Dream anymore 😍
-</HeaderTag>;
+const Header = () => (
+  <HeaderTag className="d-flex align-items-center justify-content-around">
+    <div className="d-flex align-items-center">
+      <Logo src={logo} alt="logo-img" />
+      <div className="ms-3 fs-3 fw-bold">APSignals</div>
+    </div>
+    <div className="fs-4">Learn Coding for Free is not a Dream anymore!</div>
+    <a className="btn btn-secondary fs-5" href="https://www.instagram.com/apsignal" target="/">
+      Our Instagram
+      <FaInstagram className="ms-3 fs-3" />
+    </a>
+  </HeaderTag>
+);
 
 export default Header;
--- src/components/Header/view.js
+++ src/components/Header/view.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const HeaderTag = styled.header({
-  height: 75,
+  height: 60,
   boxShadow: "0 5px 10px -2px grey",
 });
+
+export const Logo = styled.img({ width: 75 });
