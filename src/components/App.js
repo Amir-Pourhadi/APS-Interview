c3b271288e2d0a7d3a8a6a10a68469125af0942b
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-13 05:31:56 +0430

    footer and header rendered

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import { Route, Switch } from "react-router";
 import Animals from "./Animals";
+import Footer from "./Footer";
 import { GlobalStyle } from "./GlobalStyle";
+import Header from "./Header";
 import Home from "./Home";
 import Name from "./Name";
 import SortList from "./SortList";
@@ -15,14 +17,16 @@ const App = () => {
   };
 
   return (
-    <div className="container-fluid">
+    <div className="container-fluid p-0">
       <GlobalStyle />
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/name" render={(props) => <Name {...props} updateUser={updateUser} user={user} />} />
         <Route path="/animals" render={(props) => <Animals {...props} updateUser={updateUser} user={user} />} />
         <Route path="/sort-list" render={(props) => <SortList {...props} user={user} />} />
       </Switch>
+      <Footer />
     </div>
   );
 };
