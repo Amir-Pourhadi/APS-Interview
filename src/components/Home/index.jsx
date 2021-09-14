70c7719068db3a9952e343219323d15a3d4920fa
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-14 16:45:40 +0430

    typo fixed

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,8 +5,8 @@ const Home = () => (
   <Main className="text-center">
     <Heading>Welcome to APSignals English Interview</Heading>
     <p className="py-5 fs-3">
-      You are going to answer some easy questions. <br />
-      Take a deep breathe and press Continue to start ... <br />
+      You are going to answer few simple questions. <br />
+      Take a deep breath and press Continue to start ... <br />
       Please remember do NOT refresh page and complete inputs carefully!
     </p>
     <PrimaryBtn link="/name" />
