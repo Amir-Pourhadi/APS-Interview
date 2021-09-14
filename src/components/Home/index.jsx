bd9c62f3116cf211bd1961a31a6c1bd33e7f1dc6
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-14 14:17:44 +0430

    header updated

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,13 @@
 import PrimaryBtn from "../assets/PrimaryBtn";
-import { Main } from "./view";
+import { Heading, Main } from "./view";
 
 const Home = () => (
   <Main className="text-center">
-    <h1 className="py-5">Welcome to APSignlas English Interview</h1>
+    <Heading>Welcome to APSignals English Interview</Heading>
     <p className="py-5 fs-3">
-      You are going to answer some easy but nice questions. <br />
-      Take a deep breath and press Continue to start...
+      You are going to answer some easy questions. <br />
+      Take a deep breathe and press Continue to start ... <br />
+      Please remember do NOT refresh page and complete inputs carefully!
     </p>
     <PrimaryBtn link="/name" />
   </Main>
