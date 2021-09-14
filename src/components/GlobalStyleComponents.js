d4d4ccc929fea5dfced136458353e22e5910c240
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-14 21:20:04 +0430

    timer added

---
+++ src/components/GlobalStyleComponents.js
@@ -0,0 +1,9 @@
+import { FaAngleRight } from "react-icons/fa";
+import styled from "styled-components";
+
+export const Text = styled.span({ fontSize: 20 });
+
+export const AngleRight = styled(FaAngleRight)({
+  fontSize: 25,
+  marginLeft: 12,
+});
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,17 +1,36 @@
-import PrimaryBtn from "../assets/PrimaryBtn";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import { AngleRight, Text } from "../GlobalStyleComponents";
 import { Heading, Main } from "./view";
 
-const Home = () => (
-  <Main className="text-center">
-    <Heading>Welcome to APSignals English Interview</Heading>
-    <p className="py-5 fs-3">
-      You are going to answer a few simple questions. <br />
-      Take a deep breath and click on Continue to start ... <br />
-      Refreshing the page is not necessary, and fill the inputs carefully! <br />
-      Good Luck!
-    </p>
-    <PrimaryBtn link="/name" />
-  </Main>
-);
+const Home = () => {
+  const [seconds, setSeconds] = useState(13);
+  const history = useHistory();
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((seconds) => {
+        !--seconds && clearInterval(interval);
+        return seconds;
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Main className="text-center">
+      <Heading>Welcome to APSignals English Interview</Heading>
+      <p className="py-5 fs-3">
+        You are going to answer a few simple questions. <br />
+        Refreshing the page is not necessary! <br />
+        Take a deep breath and click on Continue to start ... <br />
+        Good Luck!
+      </p>
+      <button className="btn btn-primary" disabled={seconds} onClick={() => history.push("/name")}>
+        <Text>{seconds ? `Wait ${seconds} Seconds` : "Continue"}</Text>
+        {!seconds && <AngleRight />}
+      </button>
+    </Main>
+  );
+};
 export default Home;
