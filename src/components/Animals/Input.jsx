4ad8f1b8386d6d1d82b22598032921260bdcb208
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-15 16:07:39 +0430

    timer set for animals

--- src/components/Animals/Input.jsx
+++ src/components/Animals/Input.jsx
@@ -11,7 +11,13 @@ const Input = ({ content, count }) => {
   return (
     <div className="input-group input-group-lg w-50 me-auto ms-auto py-3">
       <span className="input-group-text">{`#${count + 1}`}</span>
-      <input type="text" className="form-control" value={input} ref={inputEl} onChange={handleChange} />
+      <input
+        type="text"
+        className="form-control"
+        value={input}
+        ref={inputEl}
+        onChange={handleChange}
+      />
     </div>
   );
 };
--- src/components/Animals/Swap.jsx
+++ src/components/Animals/Swap.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import { AngleRight, Heading, Text } from "../GlobalStyleComponents";
 import Input from "./Input";
 
 const Swap = ({ name, animals, setDone }) => {
+  const [seconds, setSeconds] = useState(13);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((seconds) => {
+        !--seconds && clearInterval(interval);
+        return seconds;
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <Heading>Nice Job {name}! Please swap first and last letter of each animal.</Heading>
@@ -9,9 +22,9 @@ const Swap = ({ name, animals, setDone }) => {
         {animals.map((animal, index) => (
           <Input content={animal} count={index} animals={animals} key={index} />
         ))}
-        <button className="btn btn-primary" onClick={() => setDone(true)}>
-          <Text>Continue</Text>
-          <AngleRight />
+        <button className="btn btn-primary" disabled={seconds} onClick={() => setDone(true)}>
+          <Text>{seconds ? `Wait ${seconds} Seconds` : "Continue"}</Text>
+          {!seconds && <AngleRight />}
         </button>
       </div>
     </>
