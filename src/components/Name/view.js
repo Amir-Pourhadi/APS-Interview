07f2c2b999c0b60273b81b483b0e244b05953319
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-18 20:53:24 +0430

    component moved to route

--- src/components/Name/view.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Main = styled.main({
-  height: "calc(100vh - 100px)",
-});
-
-export const Heading = styled.h1({
-  padding: "8% 0 20px",
-});
